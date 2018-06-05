da157f790dbf86bd88604836c9bba80fbc899220
aleyesa <aaron.leyesa@gmail.com>
2018-06-05 15:26:14 -0700

    foodsearch feature working

--- js/index.js
+++ js/index.js
@@ -3,36 +3,52 @@
 const handleFoodSearchRequest = (nameOfFood) => {
     const ndbSearchEndpoint = 'https://api.nal.usda.gov/ndb/search';
     const requestParam = {
-        api_key: 'API-Key',
+        api_key: 'Dque88jiOSae3F2qrzzsBSqCYrNxXMn5iydg3NLe',
         q: nameOfFood,
     };
     $.getJSON(ndbSearchEndpoint, requestParam, (data) => {
-        let foodSearchResults = data.list.item.map((foodName) => 
-        console.log('name: ' + foodName.name + '\n' + 'dbno: ' +  foodName.ndbno));
-        handleFoodReportRequest(data.list.item[0].ndbno);
+        const foodSearchResults = data.list.item.map((results) => 
+        renderResult(results)
+    );
+    $('.search-results').html(foodSearchResults);
     });
 };
 //A Food Report is a list of nutrients and their values in various portions for a specific food.
 const handleFoodReportRequest = (ndbno) => {
     const foodReportEndpoint = 'https://api.nal.usda.gov/ndb/V2/reports';
     const requestParam = {
-        api_key: 'API-Key',
+        api_key: 'Dque88jiOSae3F2qrzzsBSqCYrNxXMn5iydg3NLe',
         ndbno: ndbno
     };
     $.getJSON(foodReportEndpoint, requestParam, (data) => {
-        // let macros = data.foods.food.map((macro) => 
-        // macro);
         for(let i = 1; i < 5; i++)
         {
-        console.log(data.foods[0].food.nutrients[i]);
+            console.log(data.foods[0].food.nutrients[i]);
         }
     });
 };
 
-handleFoodSearchRequest('Burger King');
+function renderResult(result) {
+    return `
+    <h2>${result.name}</h2>
+    <div class="dropdown">
+        <span>Macros</span>
+        <div class="dropdown-content">
+            <p>Macro here</p>
+        </div>
+    </div>
+    <p>${result.ndbno}</p>
+    `;
+  }
 
-
-
-
-
-
+const handleSearch = () => {
+    $('.search-form').submit(event => {
+        event.preventDefault();
+        const queryTarget = $(event.currentTarget).find('.js-query');
+        const query = queryTarget.val();
+        // clear out the input
+        queryTarget.val("");
+        handleFoodSearchRequest(query);
+    });
+};
+handleSearch();
